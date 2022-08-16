a1b61e90c4a9d94842fa0e2a0981c59bddbe851b
gera <gera@odoo.com>
2022-08-16 12:08:40 -0500

    act

--- ar_report_invoice_date/models/__init__.py
+++ ar_report_invoice_date/models/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
-from . import ConfigurationSettings
-from . import Subscription
+
+from . import account_aged_partner_balance
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
